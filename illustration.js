f5290e7e9c08290027d3b3c097cb52a11ffd45ed
Liang <liang@LiangdeMacBook.local>
2017-10-25 21:51:25 +0800

    first commit

---
+++ illustration.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('h1').show(2000);
+    $('h1').delay(7000).hide('3000');
+    $('.content').delay(10000).show(2000);
+});
---
+++ start.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('.box1').delay(1000).show(10000);
+    $('.box2').delay(12000).show(1000);
+});
